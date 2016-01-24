c37d9619bcda638b6c0b9c4bbbd62d0a4b92ea4c
Techcable <Techcable@outlook.com>
2016-01-24 01:14:43 -0700

    Fix for custom jar support
    Uses a json file on the classpath to specify patch info

---
+++ src/main/java/com/destroystokyo/paperclip/PatchData.java
@@ -0,0 +1,70 @@
+package com.destroystokyo.paperclip;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class PatchData {
+    private final URL patchFile;
+    private final URL originalUrl;
+    private final byte[] originalHash;
+    private final byte[] patchedHash;
+
+    public PatchData(JSONObject obj) {
+        final String patch = (String) obj.get("patch");
+        // First try and parse the patch as a uri
+        URL patchFile = PatchData.class.getResource("/" + patch);;
+        if (new File(patch).exists()) {
+            try {
+                patchFile = new File(patch).toURI().toURL();
+            } catch (MalformedURLException ignored) {}
+        }
+        if (patchFile == null) {
+            throw new IllegalArgumentException("Couldn't find " + patch);
+        }
+        this.patchFile = patchFile;
+        try {
+            this.originalUrl = new URL((String) obj.get("sourceUrl"));
+        } catch (MalformedURLException e) {
+            throw new IllegalArgumentException("Invalid URL", e);
+        }
+        this.originalHash = Utils.fromHex((String) obj.get("originalHash"));
+        this.patchedHash = Utils.fromHex((String) obj.get("patchedHash"));
+    }
+
+    public URL getPatchFile() {
+        return patchFile;
+    }
+
+    public URL getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public byte[] getOriginalHash() {
+        return originalHash;
+    }
+
+    public byte[] getPatchedHash() {
+        return patchedHash;
+    }
+
+    public static PatchData parse(InputStream in) throws IOException {
+        try {
+            Object obj = new JSONParser().parse(new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8)));
+            return new PatchData((JSONObject) obj);
+        } catch (ParseException | ClassCastException e) {
+            throw new IllegalArgumentException("Invalid json", e);
+        }
+    }
+}
