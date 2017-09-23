2ff02b31e738d096a5a96f22edefa5ee21e3188b
DemonWav <demonwav@gmail.com>
2017-09-23 12:42:04 -0500

    Use System.err to more highlight the warning messages

--- java9/src/main/java/com/destroystokyo/paperclip/Main.java
+++ java9/src/main/java/com/destroystokyo/paperclip/Main.java
@@ -3,8 +3,8 @@ package com.destroystokyo.paperclip;
 public class Main {
 
     public static void main(final String[] args) {
-        System.out.println("WARNING: Java 9 currently causes significant issue for a lot of Bukkit plugins due to new ClassLoading semantics.");
-        System.out.println("WARNING: For the time being, it is recommended to stick with Java 8 until plugins are updated.");
+        System.err.println("WARNING: Java 9 currently causes significant issues for a lot of Bukkit plugins due to new ClassLoading semantics.");
+        System.err.println("WARNING: For the time being, it is recommended to stick with Java 8 until plugins are updated.");
 
         Paperclip.run(args);
     }
