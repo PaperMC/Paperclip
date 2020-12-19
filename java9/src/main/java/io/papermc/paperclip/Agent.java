2d4c7b3bbd322d8b7f3bbe2fe33ecf627251c828
Kyle Wood <demonwav@gmail.com>
2020-12-18 22:37:12 -0800

    Allow the -javaagent argument on Java 8, even though it does nothing

--- java9/src/main/java/io/papermc/paperclip/Agent.java
+++ java9/src/main/java/io/papermc/paperclip/Agent.java
@@ -29,7 +29,9 @@ public final class Agent {
     @SuppressWarnings("unused") // This class replaces the Agent class in the java8 module when run on Java9+
     static void addToClassPath(final Path paperJar) {
         if (inst == null) {
-            System.err.println("Unable to retrieve Instrumentation API to add Paper jar to ClassPath");
+            System.err.println("Unable to retrieve Instrumentation API to add Paper jar to classpath. If you're " +
+                "running paperclip without -jar then you also need to include the -javaagent:<paperclip_jar> JVM " +
+                "command line option.");
             System.exit(1);
             return;
         }
