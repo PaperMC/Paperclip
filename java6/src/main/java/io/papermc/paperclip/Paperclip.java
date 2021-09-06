8431502036fe535f1fb67d17b5a3ad733861eaa4
NotMyFault <mc.cache@web.de>
2021-09-06 11:16:31 +0200

    Link to Paper's Java guide
    Co-Authored-By: MiniDigger | Martin <2185527+MiniDigger@users.noreply.github.com>

--- java6/src/main/java/io/papermc/paperclip/Paperclip.java
+++ java6/src/main/java/io/papermc/paperclip/Paperclip.java
@@ -13,7 +13,8 @@ public final class Paperclip {
 
     public static void main(final String[] args) {
         System.err.println("Minecraft 1.17 requires running the server with Java 16 or above. " +
-            "Download Java 16 (or above) from https://adoptium.net/");
+            "Download Java 16 (or above) from https://adoptium.net/. For more information, see " +
+            "https://paper.readthedocs.io/en/latest/java-update/");
         System.exit(1);
     }
 }
