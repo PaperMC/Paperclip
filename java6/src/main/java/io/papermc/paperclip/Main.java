0a64dfac55fb919ef389af54bb6482b5203005c6
derklaro <git@derklaro.dev>
2021-11-28 17:45:17 +0100

    Do it correctly this time

--- java6/src/main/java/io/papermc/paperclip/Main.java
+++ java6/src/main/java/io/papermc/paperclip/Main.java
@@ -14,8 +14,7 @@ import java.lang.reflect.Method;
 public final class Main {
 
     public static void main(final String[] args) {
-        float javaVersion = Float.parseFloat(System.getProperty("java.class.version"));
-        if (javaVersion < 61f) {
+        if (getJavaVersion() < 17) {
             System.err.println("Minecraft 1.18 requires running the server with Java 17 or above. " +
                 "Download Java 17 (or above) from https://adoptium.net/");
             System.exit(1);
@@ -28,5 +27,17 @@ public final class Main {
         } catch (final Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static int getJavaVersion() {
+        final String version = System.getProperty("java.class.version");
+        final String[] parts = version.split("\\.");
+
+        if (parts.length == 0) {
+            throw new IllegalStateException("Could not determine version of the current JVM");
+        }
+
+        // class file versions: 1.1 = 45, 1.2 = 46, ..., 18 = 62
+        return Integer.parseInt(parts[0]) - 44;
     }
 }
