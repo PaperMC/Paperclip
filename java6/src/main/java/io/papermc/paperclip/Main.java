59cbdeecbf251a4d4c0bcd4e1916339a92950d41
MiniDigger | Martin <admin@benndorf.dev>
2023-09-16 10:28:16 +0200

    Link to docs in java version check (#65)

--- java6/src/main/java/io/papermc/paperclip/Main.java
+++ java6/src/main/java/io/papermc/paperclip/Main.java
@@ -16,7 +16,7 @@ public final class Main {
     public static void main(final String[] args) {
         if (getJavaVersion() < 17) {
             System.err.println("Minecraft 1.19 requires running the server with Java 17 or above. " +
-                "Download Java 17 (or above) from https://adoptium.net/");
+                "For information on how to update Java, see https://docs.papermc.io/misc/java-install");
             System.exit(1);
         }
 
